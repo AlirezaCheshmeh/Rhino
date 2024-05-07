7737122bb9127ffd558e3fc6934d779e49210fe6
safi.saeed1999 <safi.saeed1999@gmail.com>
2024-05-07 23:04:05 +0330

    add discount code and plan entities

---
+++ Domain/Entities/DiscountCodes/DiscountCode.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.BaseEntity;
+using Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.DiscountCodes
+{
+    public class DiscountCode:BaseEntity<long>
+    {
+        public string Code { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public int UseCount { get; set; }
+        public long UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Domain/Entities/Plans/Plan.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.BaseEntity;
+using Domain.Entities.DiscountCodes;
+using Domain.Entities.Users;
+
+namespace Domain.Entities.Plans
+{
+    public class Plan:BaseEntity<long>
+    {
+        public long UserId { get; set; }
+        public User User { get; set; }
+        public long DiscountCodeId { get; set; }
+        public DiscountCode DiscountCode { get; set; }
+        public DateTime ValidDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
